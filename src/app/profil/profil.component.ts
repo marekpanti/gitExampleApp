0a0c1adb36ea3a1028b24afcb68a5fe51fa0ccde
Marek Panti <marek.panti@mediworx.sk>
2021-05-06 15:59:32 +0200

    fix: formatting

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,21 +1,26 @@
-import { Component } from "@angular/core";
-import { switchMap } from "rxjs/operators";
-import { AuthGitService } from "../auth/auth.service";
-import { ListService } from "../list/list.service";
+import { Component } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { AuthGitService } from '../auth/auth.service';
+import { ListService } from '../list/list.service';
 
 @Component({
-    selector: 'app-profil',
-    templateUrl: './profil.component.html',
-    styleUrls: ['./profil.component.scss'],
-  })
-  export class ProfilComponent {
-      userDetail: any;
-      constructor(private auth: AuthGitService, private listService: ListService) {}
+  selector: 'app-profil',
+  templateUrl: './profil.component.html',
+  styleUrls: ['./profil.component.scss'],
+})
+export class ProfilComponent {
+  userDetail: any;
+  constructor(private auth: AuthGitService, private listService: ListService) {}
 
-      ngOnInit() {
-          this.auth.getUser().pipe(switchMap((user) => {
-              const userId = user.sub.substring(user.sub.indexOf('|') + 1); // get id of logged in User
-             return this.listService.getUserById(userId)
-          })).subscribe(d => this.userDetail = d);
-      }
+  ngOnInit() {
+    this.auth
+      .getUser()
+      .pipe(
+        switchMap((user) => {
+          const userId = user.sub.substring(user.sub.indexOf('|') + 1); // get id of logged in User
+          return this.listService.getUserById(userId);
+        })
+      )
+      .subscribe((d) => (this.userDetail = d));
   }
+}
